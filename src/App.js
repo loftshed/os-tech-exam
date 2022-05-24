6d89e44f1f64ea4bda09cddb37f9d1182d5d3ec0
Pete Löfstedt <pete.lofstedt@gmail.com>
2022-05-24 16:21:50 -0400

    interview

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(undefined);
+  const [boxIsChecked, setBoxIsChecked] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('https://reqres.in/api/users', { method: 'GET' });
+        const { data } = await response.json();
+        setData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    function compare(a, b) {
+      if (a.last_name < b.last_name) {
+        return -1;
+      }
+      if (a.last_name > b.last_name) {
+        return 1;
+      }
+      return 0;
+    }
+    const sortedData = data?.sort(compare);
+    setData(null);
+    console.log(sortedData);
+  }, [boxIsChecked]);
+
+  if (!data) return null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        id="checkbox"
+        type="checkbox"
+        onChange={(ev) => {
+          setBoxIsChecked(ev.target.checked);
+        }}
+      />
+
+      {data.map((el) => {
+        const { id, email, first_name, last_name, avatar } = el;
+        return (
+          <div key={id}>
+            <img src={avatar} alt="avatar" />
+            <span>
+              {first_name} {last_name} ({email})
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
 }
